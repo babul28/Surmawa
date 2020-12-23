19a4345eb484cb4b358830b6e796ae0aa43d04b4
miftahul lubab <miftahul.lubab20@gmail.com>
2020-12-24 00:23:10 +0700

    :recycle: refactor: wip on authentication test using lectures table

---
+++ database/factories/LectureFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lecture;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LectureFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Lecture::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'university' => 'State University of Malang',
+            'faculty' => 'Faculty of Engineering',
+            'departement' => 'Informatics Engineering Departement',
+        ];
+    }
+}
