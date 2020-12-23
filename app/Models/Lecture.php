b76de48843929235280ce0c3bd075f30790d2059
miftahul lubab <miftahul.lubab20@gmail.com>
2020-12-24 00:30:15 +0700

    :recycle: refactor: wip on password reset link test using lectures table

--- app/Models/Lecture.php
+++ app/Models/Lecture.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class Lecture extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     /**
      * Inactivated mass assignment protection
